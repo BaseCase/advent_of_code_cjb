e92dc3e00ec1a3ec4805a699c28da11a9a2429ab
Casey Brant <caseyb@splunk.com>
2023-12-04 16:13:54 -0600

    remove superfluous loop for 2023 day 04

--- 2023/day04.js
+++ 2023/day04.js
@@ -1,5 +1,5 @@
-import { apply, intersection, last, length, map, match, pipe, split, sum } from 'ramda'
-import { as_lines } from './utils.js'
+import {apply, intersection, last, length, map, match, pipe, pluck, range, split, sum} from 'ramda'
+import {as_lines, map_indexed} from './utils.js'
 
 const numbers = /(\d+)/g
 
@@ -10,23 +10,15 @@ const matching_number_counts = map(pipe(
   length
 ))
 
-function tally_cards(starting_stack) {
-  let count = 0
-  let stack = starting_stack.map((n, idx) => [idx+1, n, 1])   // [card id, card score, number of dupes]
-  while (stack.length !== 0) {
-    let [card, score, value] = stack.shift()
-    count += value
-    if (score === 0) continue
-
-    for (let i=0; i<value; ++i) {
-      for (let j=card+1; j < card+1+score; ++j) {
-        let to_dupe = stack.find(([id]) => id === j)
-        if (to_dupe === undefined) continue
-        to_dupe[2] += 1
-      }
+function tally_cards(given_cards) {
+  const cards = map_indexed((n, idx) => [idx+1, n, 1], given_cards) // [[card ID, card score, # duplicates of this card]]
+  for (let [card, score, duplicates] of cards) {
+    for (let i of range(card+1, card+1+score)) {
+      let to_dupe = cards.find(([id]) => id === i)
+      to_dupe[2] += duplicates
     }
   }
-  return count
+  return cards
 }
 
 const part01 = pipe(
@@ -39,7 +31,9 @@ const part01 = pipe(
 const part02 = pipe(
   as_lines,
   matching_number_counts,
-  tally_cards
+  tally_cards,
+  pluck(2),
+  sum
 )
 
 export const day04 = input => [part01(input), part02(input)]
