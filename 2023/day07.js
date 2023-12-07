2d7aedb9c5fb01b05e38ada2336e80ad079c486d
Casey Brant <caseyb@splunk.com>
2023-12-07 10:37:32 -0600

    2023 day07 full

--- 2023/day07.js
+++ 2023/day07.js
@@ -1,52 +1,58 @@
-import * as r from 'ramda'
-import { log, as_lines, head_lens, map_indexed } from './utils.js'
+import { apply, ascend, collectBy, identity, length, map, partition, pipe, prop, reduce, sort, sortWith, split, sum, zip } from 'ramda'
+import { as_lines, map_indexed } from './utils.js'
 
 
 const card_ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+const card_ranks_part2 = ['J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A']
 
-const hand_type = h => {
-  const set_sizes = r.pipe(r.collectBy(r.identity), r.map(r.length))(h)
-  return set_sizes.some(s => s === 5) ? 7
-    : set_sizes.some(s => s === 4) ? 6
-      : set_sizes.some(s => s === 3) && set_sizes.some(s => s === 2) ? 5
-        : set_sizes.some(s => s === 3) ? 4
-          : set_sizes.filter(s => s === 2).length === 2 ? 3
-            : set_sizes.some(s => s === 2) ? 2
+const hand_type = (h, allow_jokers) => {
+  let sets = collectBy(identity, h)
+  let num_jokers = 0
+
+  if (allow_jokers) {
+    const [jokers, normies] = partition(s => s[0] === 'J', sets)
+    num_jokers = jokers[0]?.length ?? 0
+    if (num_jokers === 5) return 7
+    sets = normies
+  }
+
+  const set_sizes = pipe(map(length), sort((a, b) => b - a))(sets)
+  set_sizes[0] += num_jokers
+
+  return set_sizes[0] === 5 ? 7
+    : set_sizes[0] === 4 ? 6
+      : set_sizes[0] === 3 && set_sizes[1] === 2 ? 5
+        : set_sizes[0] === 3 ? 4
+          : set_sizes[0] === 2 && set_sizes[1] === 2 ? 3
+            : set_sizes[0] === 2 ? 2
               : 1
 }
 
-const score_hand = ([h, b]) => ({
+const score_hand = ([h, b], allow_jokers) => ({
   hand: h,
-  type_score: hand_type(h),
-  card_scores: r.map(c => card_ranks.indexOf(c), h),
+  type_score: hand_type(h, allow_jokers),
+  card_scores: map(c => (allow_jokers ? card_ranks_part2 : card_ranks).indexOf(c), h),
   bid: Number.parseInt(b),
 })
 
-const pairwise_compare = r.pipe(
-  r.apply(r.zip),
-  r.reduce(
+const pairwise_compare = pipe(
+  apply(zip),
+  reduce(
     (comp, [a, b]) => comp !== 0 ? comp : a - b,
     0))
 
-const part01 = r.pipe(
-  as_lines, r.map(r.split(' ')),
-  r.map(score_hand),
-  r.sortWith([
-    r.ascend(r.prop('type_score')),
+const calculate_score = allow_jokers => pipe(
+  as_lines, map(split(' ')),
+  map(h => score_hand(h, allow_jokers)),
+  sortWith([
+    ascend(prop('type_score')),
     (a, b) => pairwise_compare([a.card_scores, b.card_scores]),
   ]),
   map_indexed((hand, rank) => hand.bid * (rank+1)),
-  r.sum
+  sum
 )
 
+const part01 = calculate_score(false)
+const part02 = calculate_score(true)
 
-export const day07 = input => part01(input)
-
-const example_data = `
-32T3K 765
-T55J5 684
-KK677 28
-KTJJT 220
-QQQJA 483
-`
-console.dir(day07(example_data), { depth: null })
+export const day07 = input => [part01(input), part02(input)]
