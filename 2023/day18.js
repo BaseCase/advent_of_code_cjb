1ff2fdf3004c80a2e668eaa856f1c40b98803dd7
Casey Brant <casey@caseybrant.com>
2023-12-18 20:23:11 -0600

    slightly cleaned up 2023 day 18

--- 2023/day18.js
+++ 2023/day18.js
@@ -1,57 +1,31 @@
 import * as r from 'ramda'
-import { as_lines, key_to_point, log, point_to_key } from './utils.js'
+import { as_lines } from './utils.js'
 
-
-const parse_part01 = r.pipe(
-  as_lines,
-  r.map(r.pipe(
-    r.split(' '),
-    r.adjust(1, Number.parseInt)
-  ))
-)
 
 const vectors = {
-  'U': [0, -1],
   'R': [1, 0],
   'D': [0, 1],
   'L': [-1, 0],
+  'U': [0, -1],
 }
 
-const mapping = {
-  '0': 'R',
-  '1': 'D',
-  '2': 'L',
-  '3': 'U',
-}
-
-const color2int_and_dir = r.pipe(
-  r.match(/([0-9a-f]{6})/),
-  x => {
-    let a = x[0]
-    let b = x[1]
-    if (a !== b) { log("OH")}
-    return x
-  },
-  r.head,
-  r.juxt([r.take(5), r.drop(5)]),
-  r.adjust(0, s => Number.parseInt(`0x${s}`, 16)),
-  r.adjust(1, k => mapping[k]),
-)
+const parse_part01 = r.pipe(
+  as_lines, r.map(r.pipe(
+    r.split(' '), r.adjust(1, Number.parseInt))))
 
 const parse_part02 = r.pipe(
-  as_lines,
+  parse_part01, r.pluck(2),
   r.map(r.pipe(
-    r.split(' '),
-    r.last,
-    color2int_and_dir,
-    stuff => [stuff[1], stuff[0], 'unused']
-  ))
-)
+    r.match(/([0-9a-f]{5})([0-9a-f])/),
+    r.props([1, 2]),
+    r.adjust(0, s => Number.parseInt(s, 16)),
+    r.adjust(1, k => "RDLU"[k]),
+    r.reverse)))
 
 const calculate = instructions => {
   let vertices = []
   let pos = [0,0]
-  instructions.forEach(([dir, amt, color]) => {
+  instructions.forEach(([dir, amt]) => {
     let [vecx, vecy] = vectors[dir]
     pos = [vecx*amt + pos[0], vecy*amt + pos[1]]
     vertices.push([...pos])
@@ -64,7 +38,7 @@ const calculate = instructions => {
     sum1 += BigInt(p1[0] * p2[1])
     sum2 += BigInt(p1[1] * p2[0])
   }
-  const area = (sum1 - sum2) / BigInt(2)
+  const area = BigInt(sum1 - sum2) / BigInt(2)
 
   // add the perimeter to the area for total squares dug
   const perim = r.pipe(
