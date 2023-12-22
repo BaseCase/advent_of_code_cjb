b124395fa52c461c892193754b6d9f8a6213d87f
Casey Brant <caseyb@splunk.com>
2023-12-22 15:47:21 -0600

    2023 day 22

--- 2023/day22.js
+++ 2023/day22.js
@@ -1,5 +1,5 @@
 import * as r from 'ramda'
-import { as_lines, log, point_to_key } from './utils.js'
+import { as_lines } from './utils.js'
 
 
 const parse = r.pipe(
@@ -7,28 +7,14 @@ const parse = r.pipe(
   r.map(r.pipe(
     r.split('~'),
     r.map(r.pipe(r.split(','), r.map(Number.parseInt))),
-    ([start, end]) => {
-      // NOTE: we can break the points up like this because we know two of the three dimensions of every brick are 1
-      let x1 = r.min(start[0], end[0])
-      let x2 = r.max(start[0], end[0])
-      let y1 = r.min(start[1], end[1])
-      let y2 = r.max(start[1], end[1])
-      let z1 = r.min(start[2], end[2])
-      let z2 = r.max(start[2], end[2])
-      return {
-        x1: start[0],
-        y1: start[1],
-        z1: start[2],
-        x2: end[0],
-        y2: end[1],
-        z2: end[2],
-        settled: false,
-      }
-    }
+    ([start, end]) => ({
+      x1: start[0], y1: start[1], z1: start[2],
+      x2: end[0], y2: end[1], z2: end[2],
+      settled: false,
+    })
   ))
 )
 
-// sus
 const overlapping = (b1, b2) => !(
   (b1.x1 < b2.x1 && b1.x2 < b2.x1)
   || (b2.x1 < b1.x1 && b2.x2 < b1.x1)
@@ -36,13 +22,12 @@ const overlapping = (b1, b2) => !(
   || (b2.y1 < b1.y1 && b2.y2 < b1.y1)
 )
 
-
 export const day22 = input => {
   let bricks = parse(input)
 
+  // settle bricks from starting snapshot
   bricks.sort((b1, b2) => b1.z1 - b2.z1)
   let [settled, unsettled] = r.partition(r.compose(r.equals(true), r.prop('settled')), bricks)
-
   while (unsettled.length > 0) {
     let lowest = unsettled[0]
 
@@ -60,36 +45,33 @@ export const day22 = input => {
     unsettled = stuff[1]
   }
 
-  let part01 = 0
-  for (let i=0; i<bricks.length; ++i) {
-    let deletable = true
-    let brick = bricks[i]
-    let resting_on_me = bricks.filter(b => b.z1 === brick.z2+1 && overlapping(brick, b))
-
-    resting_on_me.forEach(b => {
-      if (!deletable) return
-      let supports_b = bricks.filter(b2 => b2.z2 === brick.z2 && overlapping(b2, b))
-      if (supports_b.length === 1) {
-        deletable = false
-        return
-      }
-    })
-
-    if (deletable) ++part01
+  // make it easy to see who is directly above and below each brick
+  for (let brick of bricks) {
+    brick.supported_by = new Set(bricks.filter(b => b.z2 === brick.z1 - 1 && overlapping(brick, b)))
+    brick.directly_supports = new Set(bricks.filter(b => b.z1 === brick.z2 + 1 && overlapping(brick, b)))
   }
 
-  return part01
+  // determine how many bricks would have to move if each one were deleted
+  const count_chain_reaction = brick => {
+    let would_move = new Set([brick])
+    let q = [...brick.directly_supports]
+    while (q.length) {
+      let consider = q.shift()
+      if (r.all(b => would_move.has(b), [...consider.supported_by])) {
+        would_move.add(consider)
+        q = [...q, ...consider.directly_supports]
+      }
+    }
+
+    return would_move.size - 1
+  }
+
+  for (let brick of bricks) {
+    brick.yowza = count_chain_reaction(brick)
+  }
+
+  let part01 = r.count(b => b.yowza === 0, bricks)
+  let part02 = r.sum(r.pluck('yowza', bricks))
+
+  return [part01, part02]
 }
-
-
-const example = `
-1,0,1~1,2,1
-0,0,2~2,0,2
-0,2,3~2,2,3
-0,0,4~0,2,4
-2,0,5~2,2,5
-0,1,6~2,1,6
-1,1,8~1,1,9
-`
-
-console.log(day22(example))
