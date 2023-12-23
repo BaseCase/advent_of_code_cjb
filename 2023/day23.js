5b70040542ee897a4f2b591fc0173120895a4094
Casey Brant <casey@caseybrant.com>
2023-12-23 16:13:44 -0600

    2023 day 23 part 2

--- 2023/day23.js
+++ 2023/day23.js
@@ -1,6 +1,8 @@
 import * as r from 'ramda'
-import { as_lines, grid_map, point_to_key, log } from './utils.js'
+import { as_lines, grid_map, point_to_key, log, key_to_point } from './utils.js'
 
+
+// NOTE: part 1 is in git history; I deleted it en route to solving part 2
 
 const parse = r.pipe(
   as_lines,
@@ -16,94 +18,134 @@ const slopes = {
 }
 const slopekeys = r.keys(slopes)
 
-
 export const day23 = input => {
   const grid = parse(input)
   const start = [1,0]
-  const goal = [139,140]
 
-  const longest_path = starting_point => {
-    let steps = 1
-    let [prevX, prevY] = starting_point
-    let [atX, atY] = starting_point
+  const goal = [139, 140]
+  // const goal = [21, 22]
 
-    // follow path ,adding up steps, until we hit a fork
-    //    at the fork, find the max of the two alternatives and add that
-    //    if we hit the goal, just return our count
-    //     could memoize on key(starting_point) but the input looks manageable, so maybe not necessary?
 
-    while (true) {
-      if (atX === goal[0] && atY === goal[1]) {
-        return steps - 1
-      }
 
-      let at = grid[point_to_key([atX, atY])]
+  // find all nodes, then find costs and destinations of segments coming out of each node
+  // duplicating work here is OK since the nodes themselves will be the outer loop and we won't dynamically update the queue
+  let nodes = new Set()
+  for (let p of r.keys(grid)) {
+    if (grid[p] !== '.') continue
 
-      if (r.includes(at, slopekeys)) {
-        ++steps
-        prevX = atX
-        prevY = atY
-        atX = atX + slopes[at][0]
-        atY = atY + slopes[at][1]
-      } else {
-        let possible_moves = [
-          [atX, atY-1], [atX+1, atY], [atX, atY+1], [atX-1, atY]
-        ].filter(([x, y]) => {
-          if (x === prevX && y === prevY) return false
-          let point = grid[point_to_key([x, y])]
-          if (point === undefined || point === '#') return false
-          return !((point === '>' && x < atX)
-            || (point === '<' && x > atX)
-            || (point === 'v' && y < atY)
-            || (point === '^' && y > atY))
-        })
-
-        if (possible_moves.length === 0 || possible_moves.length > 2) {
-          throw new Error("that shouldn't happen!!")
-        }
-
-        if (possible_moves.length === 1) {
-          ++steps
-          prevX = atX
-          prevY = atY
-          atX = possible_moves[0][0]
-          atY = possible_moves[0][1]
-        } else {
-          return steps + r.max(longest_path(possible_moves[0]), longest_path(possible_moves[1]))
-        }
-      }
+    let [x, y] = key_to_point(p)
+    if (r.all(
+      ([x1, y1]) => {
+        let key = point_to_key([x1, y1])
+        let point = grid[key]
+        return point === undefined || point === '#' || slopekeys.includes(point)
+      },
+      [[x, y-1], [x+1, y], [x, y+1], [x-1, y]]
+    )) {
+      nodes.add(point_to_key([x, y]))
     }
   }
 
-  const part01 = longest_path(start)
+  let graph = {}
+  let starting_node = {}
+  let goal_node = {}
 
-  return part01
+  for (let node_id of nodes) {
+    let node = {
+      id: node_id,
+      connections: []
+    }
+    graph[node_id] = node
+
+    let [nodex, nodey] = key_to_point(node_id)
+
+    let outbound = [[nodex, nodey-1], [nodex+1, nodey], [nodex, nodey+1], [nodex-1, nodey]].filter(
+      ([x, y]) => slopekeys.includes(grid[point_to_key([x, y])]))
+
+    outbound.forEach(([x, y]) => {
+      let steps = 1
+      let [atX, atY] = [x, y]
+      let visited = new Set([node_id])
+
+      while (true) {
+        if (nodes.has(point_to_key([atX, atY]))) {
+          node.connections.push({
+            node: point_to_key([atX, atY]),
+            cost: steps
+          })
+          break
+        }
+        if (atX === start[0] && atY === start[1]) {
+          node.connections.push({
+            node: point_to_key([atX, atY]),
+            cost: steps
+          })
+          starting_node = {
+            id: point_to_key(start),
+            connections: [{ node: node.id, cost: steps}]
+          }
+          break
+        }
+        if (atX === goal[0] && atY === goal[1]) {
+          node.connections.push({
+            node: point_to_key([atX, atY]),
+            cost: steps
+          })
+          goal_node = {
+            id: point_to_key(goal),
+            connections: [{ node: node.id, cost: steps}]
+          }
+          break
+        }
+
+        visited.add(point_to_key([atX, atY]))
+
+        let next_steps = [[atX, atY-1], [atX+1, atY], [atX, atY+1], [atX-1, atY]]
+          .filter(([x, y]) => {
+            let key = point_to_key([x, y])
+            if (visited.has(key)) return false
+            let point = grid[key]
+            if (point === undefined || point === '#') return false
+            return true
+          })
+
+        if (next_steps.length > 1) {
+          throw new Error("This shouldn't have happened?")
+        }
+
+        if ((atX === start[0] && atY === start[1]) || (atX === goal[0] && atY === goal[1])) {
+          throw new Error("This shouldn't have happened?")
+        }
+
+        ++steps
+        atX = next_steps[0][0]
+        atY = next_steps[0][1]
+      }
+    })
+  }
+  graph[starting_node.id] = starting_node
+  graph[goal_node.id] = goal_node
+
+  // console.dir(graph, { depth: 3 })
+
+  const longest_path = (starting_point, seen) => {
+    let node = graph[starting_point]
+    let cost = 0
+
+    seen.add(starting_point)
+
+    let conn_costs = []
+
+    for (let conn of node.connections) {
+      if (conn.node === point_to_key(goal)) {
+        return conn.cost
+      }
+      if (seen.has(conn.node)) continue
+      conn_costs.push(conn.cost + longest_path(conn.node, new Set(seen)))
+    }
+
+    return Math.max(...conn_costs)
+  }
+
+  return longest_path(point_to_key(start), new Set([point_to_key(start)]))
 }
-
-
-const example = `
-#.#####################
-#.......#########...###
-#######.#########.#.###
-###.....#.>.>.###.#.###
-###v#####.#v#.###.#.###
-###.>...#.#.#.....#...#
-###v###.#.#.#########.#
-###...#.#.#.......#...#
-#####.#.#.#######.#.###
-#.....#.#.#.......#...#
-#.#####.#.#.#########v#
-#.#...#...#...###...>.#
-#.#.#v#######v###.###v#
-#...#.>.#...>.>.#.###.#
-#####v#.#.###v#.#.###.#
-#.....#...#...#.#.#...#
-#.#########.###.#.#.###
-#...###...#...#...#.###
-###.###.#.###v#####v###
-#...#...#.#.>.>.#.>.###
-#.###.###.#.###.#.#v###
-#.....###...###...#...#
-#####################.#
-`
-// console.log(day23(example))
